f90d3875d947a99985f8d3a449fc2c31f155a4b0
Harjot Malhi <hsmalhi94@hotmail.com>
2019-10-18 19:58:05 -0400

    Add icons to tabs in bottom tab bar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { createAppContainer } from "react-navigation";
 import { createBottomTabNavigator } from "react-navigation-tabs";
+import React from 'react';
 
 import PortfolioScreen from "./app/screens/Portfolio";
 import WatchListScreen from "./app/screens/WatchList";
@@ -7,23 +8,61 @@ import MarketsScreen from "./app/screens/Markets";
 import CommunityScreen from "./app/screens/Community";
 import ProfileScreen from "./app/screens/Profile";
 
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from "react-native-vector-icons/Feather";
+const iconSize = 24;
 
 const TabNavigator = createBottomTabNavigator(
   {
-    Portfolio: { screen: PortfolioScreen },
-    WatchList: { screen: WatchListScreen },
-    Markets: { screen: MarketsScreen },
-    Community: { screen: CommunityScreen },
-    Profile: { screen: ProfileScreen }
+    Portfolio: {
+      screen: PortfolioScreen,
+      navigationOptions: {
+        tabBarLabel: "Portfolio",
+        // tabBarIcon: () => <Icon name="dollar-sign" size={iconSize} />
+      }
+    },
+    WatchList: {
+      screen: WatchListScreen,
+      navigationOptions: {
+        tabBarLabel: "Watch List",
+        tabBarIcon: () => <Icon name="eye" size={iconSize} />
+      }
+    },
+    Markets: {
+      screen: MarketsScreen,
+      navigationOptions: {
+        tabBarLabel: "Markets",
+        tabBarIcon: () => <Icon name="trending-up" size={iconSize} />
+      }
+    },
+    Community: {
+      screen: CommunityScreen,
+      navigationOptions: {
+        tabBarLabel: "Community",
+        tabBarIcon: () => <Icon name="message-circle" size={iconSize} />
+      }
+    },
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: {
+        tabBarLabel: "Profile",
+        tabBarIcon: () => <Icon name="user" size={iconSize} />
+      }
+    }
   },
-  // {
-  //   defaultNavigationOptions: ({ navigation }) => ({
-  //     tabBarIcon: () => {
-  //       return <Icon name="rocket" size={30} color="#900" />;
-  //     },
-  //   })
-  // }
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused }) => {
+        const { routeName } = navigation.state;
+        let iconName: string;
+        if (routeName === 'Portfolio') {
+          iconName = 'dollar-sign';
+        }
+        //Set up code to set up other icons based on the routeName
+
+        return <Icon name={iconName} size={iconSize} />;
+      }
+    })
+  }
 );
 
 export default createAppContainer(TabNavigator);
