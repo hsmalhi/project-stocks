935e8e47da1185f53e5580afbc77fa722cd2955e
Harjot Malhi <hsmalhi94@hotmail.com>
2019-11-24 10:00:35 -0500

    Slightly change info displayed on stock profile page

--- app/components/StockProfile.tsx
+++ app/components/StockProfile.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from "react-native";
 import { Text } from 'react-native-elements';
 import getByIndex from "../utils/helpers";
@@ -10,14 +10,19 @@ interface StockProfileProps {
 }
 
 export default function StockProfile(props: StockProfileProps) {
-  if (props.data) {
-    console.log(props.data);
+  const [ current, setCurrent ] = useState(null);
+  const [ initial, setInitial ] = useState(null);
+
+  if (props.data && !current) {
+    setCurrent(getByIndex(props.data, 0));
+    const currentDate = Object.keys(props.data)[0].split(" ")[0];
+    setInitial(props.data[`${currentDate} 09:35:00`]);
   }
 
   return (
     <View>
-      {/* <Text style={ Styles.HeaderStyles }>{props.data && props.data.close}</Text>
-      <Text style={ Styles.SubheaderStyles }>{props.data && props.data.open}</Text> */}
+      <Text style={ Styles.HeaderStyles }>{current && Math.round(current["4. close"]*100)/100}</Text>
+      <Text style={ Styles.SubheaderStyles }>{current && Math.round((Number(current["4. close"]) - Number(initial["1. open"]))*100)/100}</Text>
     </View>
   );
 }
